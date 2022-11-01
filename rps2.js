7effb1d95bf65222693c902391ec0bd49837689b
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-31 23:09:16 -0400

    Refactoring javascript code

--- rps2.js
+++ rps2.js
@@ -1,18 +1,36 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const output_player = document.getElementById("update-player");
+const output_computer = document.getElementById("update-computer");
+const computer_move = document.getElementById("computers-move")
+const status = document.getElementById("status")
+//set player scores
+let player_score = 0;
+let computer_score = 0;
 
-function rock_function () {
-    console.log("rock");
+function getComputerChoice() {
+    let move = Math.floor((Math.random() * 3)) ; 
+
+    if (move == 0) {return "rock";}
+    else if (move == 1) {return "paper";}
+    else {return "scissors";}
 }
 
-function paper_function () {
-    console.log("paper");
+function game (playermove, computermove){
+    if (playermove == computermove) {return "tie"}
+    if (playermove == "rock" && computermove == "scissors"){return "player";}
+    if (playermove == "rock" && computermove == "paper"){return "computer";}
+    if (playermove == "scissors" && computermove == "paper"){return "player";}
+    if (playermove == "scissors" && computermove == "rock"){return "computer";}
+    if (playermove == "paper" && computermove == "rock"){return "player";}
+    if (playermove == "paper" && computermove == "scissors"){return "computer";}
 }
 
-function scissors_function () {
-    console.log("scissors");
-}
+
+
+
+
 
 
 rock.addEventListener('click', rock_function);
